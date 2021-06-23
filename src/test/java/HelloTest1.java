0808a3d9a09c38e8648c9ddd219f33313dc1b9ff
h-i-erdem <hllbrhmrdm@gmail.com>
2021-06-24 00:45:31 +0200

    Added second update

--- src/test/java/HelloTest1.java
+++ src/test/java/HelloTest1.java
@@ -1,5 +1,6 @@
 public class HelloTest1 {
     public static void main(String[] args) {
         System.out.println("HelloTest1");
+        System.out.println("About to share project on GitHub");
     }
 }
